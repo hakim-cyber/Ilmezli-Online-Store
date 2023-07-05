90078d61e18a5f411152c1b01aebd337a4bcb11a
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-07-05 13:43:36 +0400

    Initial Commit

---
+++ Ilmezdi/IlmezdiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IlmezdiApp.swift
+//  Ilmezdi
+//
+//  Created by aplle on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct IlmezdiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
