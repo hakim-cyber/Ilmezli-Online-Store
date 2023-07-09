75c64e05e2479f5761056c9490c5e321a72f1f2f
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-07-09 23:57:27 +0400

    image slider

--- Ilmezdi/ProductFull/Product_ImageSlider.swift
+++ Ilmezdi/ProductFull/Product_ImageSlider.swift
@@ -9,13 +9,68 @@ import SwiftUI
 
 struct Product_ImageSlider: View {
     var images:[Image]
+    
+    @State private var screen = UIScreen.main.bounds
+    @State private var indexOfImage = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 0){
+            HStack{
+                Image(systemName: "chevron.left")
+                    .onTapGesture {
+                        if indexOfImage != 0 {
+                            withAnimation(.easeInOut){
+                                indexOfImage -=  1
+                            }
+                        }
+                    }
+                
+                ZStack{
+                    Color.gray.opacity(0.3)
+                    
+                    images[indexOfImage]
+                        .resizable()
+                        .scaledToFit()
+                        .transition(.slide)
+                    VStack{
+                        Spacer()
+                        HStack{
+                            ForEach(0..<images.count , id: \.self){num in
+                                Circle()
+                                    .fill(num == indexOfImage ? .white : .secondary)
+                                    .frame(width: 8)
+                                    .transition(.slide)
+                                    
+                            }
+                        }
+                    }
+                    .padding(.bottom)
+                    
+                }
+                .cornerRadius(7)
+                .frame(width: screen.width * 0.85,height: screen.height * 0.38)
+                
+                
+                Image(systemName: "chevron.right")
+                    .onTapGesture {
+                        if indexOfImage != images.count - 1 {
+                            withAnimation(.easeInOut){
+                                indexOfImage +=  1
+                            }
+                        }
+                    }
+                
+            }
+            
+        }
+            
+    
+        
     }
 }
 
 struct Product_ImageSlider_Previews: PreviewProvider {
     static var previews: some View {
-        Product_ImageSlider(images: [Image(uiImage: UIImage()),Image(uiImage: UIImage()),Image(uiImage: UIImage()),Image(uiImage: UIImage())])
+        Product_ImageSlider(images: [Image("image"),Image("image2"),Image("image3"),Image("image4")])
     }
 }
