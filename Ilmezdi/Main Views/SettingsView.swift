382a012b7e8b911c65e82e8449731324c937ef64
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-07-12 12:10:43 +0400

    Password Control for Admin Features

--- Ilmezdi/Main Views/SettingsView.swift
+++ Ilmezdi/Main Views/SettingsView.swift
@@ -10,13 +10,60 @@ import SwiftUI
 struct SettingsView: View {
     @EnvironmentObject var productsData:ProductsData
    @AppStorage("isAdmin") var isAdmin = false
+    @State private var password = "5555"
+    
+    @State private var writtenPassword = ""
+   @State private var showPasswordControl = false
     var body: some View {
         ZStack{
             Color.gray.opacity(0.1).ignoresSafeArea()
             Form{
-                
+                Section{
+                    if !isAdmin{
+                        Button{
+                            if !isAdmin{
+                                withAnimation {
+                                    showPasswordControl = true
+                                }
+                            }
+                        }label: {
+                            HStack{
+                                Spacer()
+                                Text("Daxil ol")
+                                Spacer()
+                            }
+                        }
+                    }else{
+                        
+                    }
+                }header: {
+                    HStack{
+                        Text("Admin Paneli")
+                        Spacer()
+                        if isAdmin{
+                            Text("✅")
+                        }else{
+                            Text("📛")
+                        }
+                            
+                    }
+                }
             }
+            .scrollContentBackground(.hidden)
+            .alert("Admin Paneline Giris", isPresented: $showPasswordControl){
+                SecureField("Parol", text: $writtenPassword)
+                Button("Gir"){
+                    if writtenPassword.trimmingCharacters(in: .whitespacesAndNewlines) == password{
+                        isAdmin = true
+                    }
+                }
+            }
+           
+            
         }
+            
+            
+        
     }
 }
 
